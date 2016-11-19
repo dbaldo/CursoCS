7fad563bc2ae01afd4dc1e9709ad2769dc47b449
dbaldo <dalbaldo@gmail.com>
2016-11-19 14:38:29 -0200

    exercicio operador

--- Exemplos/Exemplo_ADONet/Exemplo_ADONet/Program.cs
+++ Exemplos/Exemplo_ADONet/Exemplo_ADONet/Program.cs
@@ -31,11 +31,84 @@ namespace Exemplo_ADONet
             //    Console.WriteLine("{0} - {1}", row["ID_MARCA"], row["NM_MARCA"]);
             //}
 
-            foreach (var marca in repos.ConsultarMarcas())
+            //foreach (var marca in repos.ConsultarMarcas())
+            //{
+            //    Console.WriteLine("{0} - {1}", marca.Id, marca.Nome);
+            //}
+
+            MenuOperador();
+
+        }
+
+        private static void MenuOperador()
+        {
+            while (true)
             {
-                Console.WriteLine("{0} - {1}", marca.Id, marca.Nome);
+                ExibirOperadores();
+
+                Console.WriteLine("Digite o id do operador para alterar ou N para criar um novo");
+                Console.WriteLine("ESC para Sair");
+                Console.WriteLine();
+
+                ConsoleKeyInfo key = Console.ReadKey();
+                if (key.Key == ConsoleKey.Escape)
+                {
+                    break;
+                }
+                else if(key.Key == ConsoleKey.N)
+                {
+                    MenuInserirOperador();
+                }
+                else
+                {
+                    MenuAlterarOperador(key.KeyChar);
+                }
             }
 
         }
+
+        private static void ExibirOperadores()
+        {
+            Console.Clear();
+            var repos = new RepositorioOperador();
+            foreach (var op in repos.ConsultarOperadores())
+            {
+                Console.WriteLine("ID {0} - NOME {1} - LOGIN {2}", op.Id, op.Nome, op.Login);
+            }
+        }
+
+        private static void MenuAlterarOperador(char charId)
+        {
+            var id = Convert.ToInt32(charId.ToString());
+
+            var repos = new RepositorioOperador();
+
+            Console.WriteLine("===ALTERANDO===");
+
+            Console.WriteLine("ID = {0}", id);
+            Console.Write("Digite o nome ");
+            string nome = Console.ReadLine();
+            Console.Write("Digite o login ");
+            string login = Console.ReadLine();
+
+            repos.AlterarOperador(new Operador() { Id = id, Nome = nome, Login = login });
+
+        }
+
+        private static void MenuInserirOperador()
+        {
+            var repos = new RepositorioOperador();
+
+            Console.WriteLine("===INSERINDO===");
+
+            Console.Write("Digite o nome ");
+            string nome = Console.ReadLine();
+            Console.Write("Digite o login ");
+            string login = Console.ReadLine();
+
+            repos.InserirOperador(new Operador() { Nome = nome, Login = login });
+        }
+
+
     }
 }
---
+++ Exemplos/Exemplo_ADONet/Exemplo_ADONet/Repositorio/RepositorioOperador.cs
@@ -0,0 +1,74 @@
+﻿using Dominio;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exemplo_ADONet.Repositorio
+{
+    class RepositorioOperador
+    {
+        const string KConnString = @"Data Source=(LocalDB)\v11.0;AttachDbFilename=C:\Users\denis.baldo\Source\Repos\CursoCS\Exemplos\Database\lavaDB.mdf;Integrated Security=True;Connect Timeout=30";
+
+        public void InserirOperador(Operador novo)
+        {
+            //Conectar no banco
+            using (var conn = new SqlConnection(KConnString))
+            {
+                conn.Open();
+
+                var cmd = new SqlCommand("INSERT INTO TB_OPERADOR VALUES (@nome, @login) \n SELECT CAST(SCOPE_IDENTITY() AS INT)", conn);
+                cmd.Parameters.AddWithValue("@nome", novo.Nome);
+                cmd.Parameters.AddWithValue("@login", novo.Login);
+
+                novo.Id = (int)cmd.ExecuteScalar();
+
+                conn.Close();
+            }
+        }
+
+        public void AlterarOperador(Operador novo)
+        {
+            //Conectar no banco
+            using (var conn = new SqlConnection(KConnString))
+            {
+                conn.Open();
+
+                var cmd = new SqlCommand("UPDATE TB_OPERADOR SET NM_OPERADOR = @nome,  NM_LOGIN = @login WHERE ID_OPERADOR = @id", conn);
+                cmd.Parameters.AddWithValue("@nome", novo.Nome);
+                cmd.Parameters.AddWithValue("@login", novo.Login);
+                cmd.Parameters.AddWithValue("@id", novo.Id);
+
+                cmd.ExecuteNonQuery();
+
+                conn.Close();
+            }
+        }
+
+        public IEnumerable<Operador> ConsultarOperadores()
+        {
+            var list = new List<Operador>();
+
+            //Conectar no banco
+            using (var conn = new SqlConnection(KConnString))
+            {
+                conn.Open();
+
+                var cmd = new SqlCommand("SELECT ID_OPERADOR, NM_OPERADOR, NM_LOGIN FROM TB_OPERADOR", conn);
+                using (var dr = cmd.ExecuteReader())
+                {
+                    while (dr.Read())
+                    {
+                        list.Add(new Operador() { Id = dr.GetInt32(0), Nome = dr.GetString(1), Login = dr.GetString(2) });
+                    }
+                }
+            }
+
+            return list;
+        }
+
+
+    }
+}
